02fcb23ccf863e60649f1365828a7378a225ddc9
Dean Whillier <dean@whillier.com>
2020-11-15 18:18:59 -0500

    fix paths

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import * as action_types from 'action_types';
-import * as actions from 'actions';
-import * as client from 'client';
-import * as constants from 'constants';
-import * as reducers from 'reducers';
-import * as selectors from 'selectors';
-import * as store from 'store';
-import * as types from 'types';
-import * as utils from 'utils';
+import * as action_types from './action_types';
+import * as actions from './actions';
+import * as client from './client';
+import * as constants from './constants';
+import * as reducers from './reducers';
+import * as selectors from './selectors';
+import * as store from './store';
+import * as types from './types';
+import * as utils from './utils';
 
 
 export {
